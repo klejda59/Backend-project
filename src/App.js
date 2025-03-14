3a8310f89fcac3be7954ba8a056940b89db44afa
klejda59 <klejda.mamaj@hyperisland.se>
2025-03-14 09:49:13 +0100

    last changes

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import HomePage from './components/HomePage/HomePage';
 import MealDetail from './components/MealDetail/MealDetail';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import './App.css'; // Make sure to create this CSS file
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app-container">
         <header>
           <Navbar />
-          {}
         </header>
-        <main>
+        <main className="content">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/meal/:id" element={<MealDetail />} />
           </Routes>
         </main>
-        <Footer /> {}
+        <Footer />
       </div>
     </Router>
   );
 }
 
 export default App;
+
